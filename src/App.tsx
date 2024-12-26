0ccd681dab05044204391e817edb9b5a8d895185
ericvenom99 <prevenakhtar@mail.com>
2024-12-26 07:33:33 +0100

    created the privacy route for the login user

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,50 @@
-import { Routes, Route } from 'react-router-dom';
-import './globals.css';
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages';
-import AuthLayout from './_auth/AuthLayout';
-import RootLayout from './_root/RootLayout';
-import { Toaster } from './components/ui/toaster';
-
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import { Explore } from "./_root/pages";
+import { Saved } from "./_root/pages";
+import { AllUsers } from "./_root/pages";
+import { CreatePost } from "./_root/pages";
+import { EditPost } from "./_root/pages";
+import { PostDetails } from "./_root/pages";
+import { Profile } from "./_root/pages";
+import { PostDetails } from "./_root/pages";
+import { UpdateProfile } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Toaster } from "./components/ui/toaster";
 
 const App = () => {
   return (
     <div>
-        <main className="flex h-screen">
-            
-            <Routes>
-                {/* public routes */}
-                <Route element={<AuthLayout />}>
-                    <Route path="/sign-in" element={<SigninForm />}/>
-                    <Route path="/sign-up" element={<SignupForm />}/>        
-                </Route>
-                
-                {/* private routes */}
-                <Route element={<RootLayout />}>
-                    <Route index element={<Home />} />
-                </Route>
-            </Routes>
+      <main className="flex h-screen">
+        <Routes>
+          {/* public routes */}
+          <Route element={<AuthLayout />}>
+            <Route path="/sign-in" element={<SigninForm />} />
+            <Route path="/sign-up" element={<SignupForm />} />
+          </Route>
 
-            <Toaster />
+          {/* private routes */}
+          <Route element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/saved" element={<Saved />} />
+            <Route path="/all-users" element={<AllUsers />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post/:id" element={<EditPost />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+            <Route path="/profile/:id/*" element={<Profile />} />
+            <Route path="/update-profile/:id" element={<UpdateProfile />} />
+          </Route>
+        </Routes>
 
-        </main>
+        <Toaster />
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

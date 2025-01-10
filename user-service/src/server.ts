088db6be2d0f4dcd3f273ab01fc21e6c52edf330
Mornark <monarchofaether@gmail.com>
2025-01-10 21:17:48 +0300

    CORS policy code updated to allow both localhost and hosting platform Vercel

--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -5,19 +5,30 @@ import express from 'express';
 import connectDB from './config/db';
 import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/userRoutes';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
 import { errorHandler } from './middlewares/errorHandler';
 import cors from 'cors';
 import postRoutes from './routes/postRoutes';
 
-
 const app = express();
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}))
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://devlounge.vercel.app'
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin, like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,12 +38,12 @@ connectDB();
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/posts', postRoutes);
-   
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

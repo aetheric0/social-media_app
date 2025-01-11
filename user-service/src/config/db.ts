7e0ba2c59c4cb0da0aa1f395bba75cdb2be245d0
Mornark <monarchofaether@gmail.com>
2025-01-11 15:14:08 +0300

    Not logging the connections string for mongo

--- user-service/src/config/db.ts
+++ user-service/src/config/db.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        console.log('MongoDB URI:', process.env.MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI!);
         console.log('MongoDB connected')
     } catch (err) {

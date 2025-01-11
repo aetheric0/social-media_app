4b5027cb4dd9b31325d45c930719b0a6511feee6
Mornark <monarchofaether@gmail.com>
2025-01-11 15:03:34 +0300

    Logging the connection string

--- user-service/src/config/db.ts
+++ user-service/src/config/db.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
+        console.log('MongoDB URI:', process.env.MONGO_URI);
         await mongoose.connect(process.env.MONGO_URI!);
         console.log('MongoDB connected')
     } catch (err) {
